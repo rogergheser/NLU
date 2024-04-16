cffd5eef6a132f30c0490fb51b26c039b9cdcbd2
Amir <amir.gheser@studenti.unitn.it>
2024-04-16 11:50:15 +0200

    asgd

--- project.py
+++ project.py
@@ -20,6 +20,8 @@ def get_model(task, vocab_len, pad_index):
         return LM_LSTM(emb_size=300, hidden_size=300, output_size=vocab_len, pad_index=pad_index, out_dropout=.2, emb_dropout=.65, n_layers=1, weight_tying=True, variational_dropout=True)
     elif task == '22':
         return LM_LSTM(emb_size=600, hidden_size=600, output_size=vocab_len, pad_index=pad_index, out_dropout=.3, emb_dropout=.65, n_layers=1, weight_tying=True, variational_dropout=True)
+    elif task == '23':
+        return LM_LSTM(emb_size=600, hidden_size=600, output_size=vocab_len, pad_index=pad_index, out_dropout=.3, emb_dropout=.65, n_layers=2, weight_tying=True, variational_dropout=True)
     else:
         raise ValueError("Model not found")
 
@@ -72,7 +74,15 @@ def main(task, device='cuda:0'):
         lr = .003
         optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
     elif task == '23':
-        raise SystemError("Should not be in this function")
+        clip = 5
+        lr = 3
+        optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+        k = 0
+        t = 0
+        T = 0
+        L = 5 # number of iterations in epoch
+        monotone = 5
+        logs = []
     else:
         raise ValueError("Task not found")
 
@@ -87,15 +97,30 @@ def main(task, device='cuda:0'):
     best_ppl = math.inf
     best_model = None
     pbar = tqdm(range(1,n_epochs))
+    stored_loss = float('inf')
     #If the PPL is too high try to change the learning rate
     for epoch in pbar:
         loss = train_loop(train_loader, optimizer, criterion_train, model, clip)
-
+        
         if epoch % 1 == 0:
+            if task == '23':
+                ppl_dev, loss_dev = eval_loop(dev_loader, criterion_eval, model)
+                losses_dev.append(np.asarray(loss_dev).mean())
+                logs.append(ppl_dev)
+                if loss_dev < stored_loss:
+                    stored_loss = loss_dev
+                    ###
+                if len(logs) > monotone and loss_dev > min(logs[:-monotone-1]):
+                    print("Using ASGD")
+                    optimizer = torch.optim.ASGD(model.parameters(), lr=lr, t0=0, lambd=0., weight_decay=0)
+                
+                #### paper implements weight decay here
+            else:
+                ppl_dev, loss_dev = eval_loop(dev_loader, criterion_eval, model)
+                losses_dev.append(np.asarray(loss_dev).mean())
+            
             sampled_epochs.append(epoch)
             losses_train.append(np.asarray(loss).mean())
-            ppl_dev, loss_dev = eval_loop(dev_loader, criterion_eval, model)
-            losses_dev.append(np.asarray(loss_dev).mean())
             pbar.set_description("PPL: %f" % ppl_dev)
             if  ppl_dev < best_ppl: # the lower, the better
                 best_ppl = ppl_dev
